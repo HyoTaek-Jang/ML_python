925c539cc85ff31e2091913e46c6b3b2800a9aa5
HyoTaekJang <hyotaek9812@gmail.com>
2021-01-05 13:57:16 +0900

    이산수학 과제 완료

--- LinearAlgebra/LA_Assignment/basic_linear_algebra.py
+++ LinearAlgebra/LA_Assignment/basic_linear_algebra.py
@@ -29,39 +29,65 @@ print(scalar_vector_product(5,[1,2,3]))
 def matrix_size_check(*matrix_variables):
     return len(set([len(matrix) for matrix in matrix_variables])) == 1 and len(set([len(matrix[0]) for matrix in matrix_variables])) == 1
 
-a= [[1,2,3],[1,1,1]]
-b= [[2,2,2],[1,1,3],[1,2,3]]
+a = [[2,3],[2,2]]
+b = [[2,3],[2,2]]
+
 print(matrix_size_check(a,b))
 
 def is_matrix_equal(*matrix_variables):
-    return None
+    # 아래 함수에 인자로 넘기면 튜플이 또 감싸져서 함수가 제대로 작동을 안함.
+    if matrix_size_check(*matrix_variables) == False:
+        raise ArithmeticError
+    # return all([ set(ele) == 1 for row in matrix_variables for ele in zip(*row) for ele in zip(*ele)])
+    return all([ len(set(ele))==1 for matrix in matrix_variables for row in zip(*matrix) for ele in zip(*row)])
 
+print(is_matrix_equal((a,b)))
 
 def matrix_addition(*matrix_variables):
     if matrix_size_check(*matrix_variables) == False:
         raise ArithmeticError
-    return None
+    return [[sum(b) for b in zip(*a)] for a in zip(*matrix_variables)]
 
+print(matrix_addition(a,b))
 
 def matrix_subtraction(*matrix_variables):
     if matrix_size_check(*matrix_variables) == False:
         raise ArithmeticError
-    return None
+    return [[2*b[0]-sum(b) for b in zip(*a)] for a in zip(*matrix_variables)]
 
+a = [[9,3],[9,2]]
+b = [[1,2],[1,2]]
+c = [[5,7],[2,1]]
+
+print(matrix_subtraction(a,b,c))
 
 def matrix_transpose(matrix_variable):
-    return None
+    return [[*a] for a in zip(*matrix_variable)]
 
+a = [[9,3],[9,2],[1,1],[7,7]]
+
+print(matrix_transpose(a))
 
 def scalar_matrix_product(alpha, matrix_variable):
-    return None
+    return [[alpha*b for b in a]for a in matrix_variable]
 
+print(scalar_matrix_product(5,a))
 
 def is_product_availability_matrix(matrix_a, matrix_b):
-    return None
+    print(len([b for b in zip(*matrix_b)][0]))
+    print(len(matrix_a))
+    return len(matrix_a[0]) == len([b for b in zip(*matrix_b)][0])
+    # return len(set(zip(*matrix_b[0],zip(matrix_a)[0]))) == 1
+
+d= [[1,2],[2,2],[3,3],[1,1]]
+print(is_product_availability_matrix(a,d))
 
 
 def matrix_product(matrix_a, matrix_b):
     if is_product_availability_matrix(matrix_a, matrix_b) == False:
         raise ArithmeticError
-    return None
+    return [[sum([c*d for c,d in zip(a,b)]) for b in zip(*matrix_b)] for a in matrix_a]
+f = [[1,1,2],[2,1,1]]
+g = [[2,4],[5,3],[1,3]]
+
+print(matrix_product(f,g))
